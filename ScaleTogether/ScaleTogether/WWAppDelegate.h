7d29289d23a87ccc49dee80e5dfd868f5ccefab5
Wee W <weerasak@gmail.com>
2013-02-10 08:57:56 -0600

    Wee-Created an xcode project.

---
+++ ScaleTogether/ScaleTogether/WWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WWAppDelegate.h
+//  ScaleTogether
+//
+//  Created by Wee Witthawaskul on 2/10/13.
+//  Copyright (c) 2013 Wee Witthawaskul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScaleTogether/ScaleTogether/WWViewController.h
@@ -0,0 +1,13 @@
+//
+//  WWViewController.h
+//  ScaleTogether
+//
+//  Created by Wee Witthawaskul on 2/10/13.
+//  Copyright (c) 2013 Wee Witthawaskul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWViewController : UIViewController
+
+@end
