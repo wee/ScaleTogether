7ee8a375b7fbbcc4be21e9a05ddbd29ae45294af
Wee W <weerasak@gmail.com>
2013-02-11 12:37:48 -0600

    Wee-Added pan gesture recognizer in scale wheel.

--- ScaleTogether/ScaleTogether/Views/WWScaleView.m
+++ ScaleTogether/ScaleTogether/Views/WWScaleView.m
@@ -13,18 +13,68 @@
 #define MAJOR_TICK_LENGTH 30.0
 #define MINOR_TICK_LENGTH 10.0
 
-@implementation WWScaleView
+#define NUMBER_OF_TICKS 48
 
-static CGFloat rotationDegree  = 0.0;
+@interface WWScaleView()<UIGestureRecognizerDelegate>
+
+@property (nonatomic, assign) CGFloat rotationDegree;
+@property (nonatomic, assign) CGPoint beginPanPoint;
+
+@end
+
+@implementation WWScaleView
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
+        self.rotationDegree = 0.0;
+        UIGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onPanGesture:)];
+        recognizer.delegate = self;
+        [self addGestureRecognizer:recognizer];
     }
     return self;
 }
 
+#pragma mark - Pan Gesture Recognizer
+- (BOOL)gestureRecognizerShouldBegin:(UIPanGestureRecognizer *)gestureRecognizer
+{
+    CGPoint translation = [gestureRecognizer translationInView:[self superview]];
+    if (fabsf(translation.x) > fabsf(translation.y)) {
+        return YES;
+    }
+    return NO;
+}
+
+- (void)onPanGesture:(UIPanGestureRecognizer *)recognizer
+{
+    switch (recognizer.state) {
+        case UIGestureRecognizerStateBegan:
+            self.beginPanPoint = [recognizer translationInView:self];
+            NSLog(@"start at %@", NSStringFromCGPoint(self.beginPanPoint));
+            break;
+        case UIGestureRecognizerStateChanged:
+        {
+            CGPoint newPoint = [recognizer translationInView:self];
+            if (fabs(newPoint.x - self.beginPanPoint.x) > 5.0) {
+                self.rotationDegree += (newPoint.x - self.beginPanPoint.x) / 100;
+                self.beginPanPoint = newPoint;
+                [self setNeedsDisplay];
+            }
+            break;
+        }
+        case UIGestureRecognizerStateEnded:
+        {
+            CGPoint newPoint = [recognizer translationInView:self];
+            NSLog(@"end at %@", NSStringFromCGPoint(newPoint));
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+#pragma mark - Draw
 - (void)drawRect:(CGRect)rect
 {
     [self.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
@@ -37,17 +87,22 @@ static CGFloat rotationDegree  = 0.0;
     
     CGContextFillPath(context);
     
-    NSUInteger numberOfSections = 48;
-    for (NSUInteger section = 0; section < numberOfSections; section++) {
-        [self drawTickAndLabel:section sections:numberOfSections startingRadius:rotationDegree context:context];
+    for (NSUInteger section = 0; section < NUMBER_OF_TICKS; section++) {
+        [self drawTickAndLabel:section sections:NUMBER_OF_TICKS startingRadius:self.rotationDegree context:context];
     }
+    
+    UIButton *recordButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    CGFloat recordButtonWidh = 80.0;
+    recordButton.frame = CGRectMake(size.width/2.0-(recordButtonWidh/2), size.height/2, recordButtonWidh, 40);
+    [recordButton setTitle:@"Record" forState:UIControlStateNormal];
+    [self addSubview:recordButton];
     // Test scale wheel rotation
-    [NSTimer scheduledTimerWithTimeInterval:0.2 target:self selector:@selector(rotate) userInfo:nil repeats:NO];
+    //[NSTimer scheduledTimerWithTimeInterval:0.2 target:self selector:@selector(rotate) userInfo:nil repeats:NO];
 }
 
 - (void)rotate
 {
-    rotationDegree += 0.01;
+    self.rotationDegree += 0.01;
     [self setNeedsDisplay];
     
 }
@@ -66,18 +121,15 @@ static CGFloat rotationDegree  = 0.0;
     
     CGFloat tickLength = (section % 2) == 0 ? MAJOR_TICK_LENGTH : MINOR_TICK_LENGTH;
     CGFloat angle = angleSize * section + startingRadius;
-    CGFloat x = midX + sin(angle) * WHEEL_RADIUS;
-    CGFloat y = WHEEL_RADIUS - cos(angle) * WHEEL_RADIUS;
-    CGFloat x2 = midX + sin(angle) * (WHEEL_RADIUS - tickLength);;
-    CGFloat y2 = WHEEL_RADIUS - cos(angle) * (WHEEL_RADIUS - tickLength);
-    CGContextMoveToPoint(context, x, y);
-    CGContextAddLineToPoint(context, x2, y2);
+    CGContextMoveToPoint(context,
+                         midX + sin(angle) * WHEEL_RADIUS,
+                         WHEEL_RADIUS - cos(angle) * WHEEL_RADIUS);
+    CGContextAddLineToPoint(context,
+                            midX + sin(angle) * (WHEEL_RADIUS - tickLength),
+                            WHEEL_RADIUS - cos(angle) * (WHEEL_RADIUS - tickLength));
     CGContextStrokePath(context);
     CGColorSpaceRelease(colorspace);
     CGColorRelease(color);
-    
-    UILabel *im = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 40)];
-    im.backgroundColor = [UIColor clearColor];
     
     CGFloat value;
     if (section < numberOfSections/2) {
@@ -85,13 +137,20 @@ static CGFloat rotationDegree  = 0.0;
     } else {
         value = [self.weight floatValue] - ((CGFloat)numberOfSections - section) / 2;
     }
-    
-    im.text = [NSString stringWithFormat:@"%.1f", value];
-    im.textAlignment = NSTextAlignmentCenter;
-    im.layer.anchorPoint = CGPointMake(0.5f, 0.5f);
-    im.layer.position = CGPointMake(midX, WHEEL_RADIUS);
-    im.transform = CGAffineTransformConcat(CGAffineTransformMakeTranslation(0, -WHEEL_RADIUS + MAJOR_TICK_LENGTH + 10.0), CGAffineTransformMakeRotation(angle));
-    [self addSubview:im];
+    [self addSubview:[self assignTickLabel:value angle:(CGFloat)angle]];
+}
 
+- (UILabel *)assignTickLabel:(CGFloat)value angle:(CGFloat)angle
+{
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 40)];
+    label.backgroundColor = [UIColor clearColor];
+    
+    
+    label.text = [NSString stringWithFormat:@"%.1f", value];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.layer.anchorPoint = CGPointMake(0.5f, 0.5f);
+    label.layer.position = CGPointMake(self.bounds.size.width/2.0, WHEEL_RADIUS);
+    label.transform = CGAffineTransformConcat(CGAffineTransformMakeTranslation(0, -WHEEL_RADIUS + MAJOR_TICK_LENGTH + 10.0), CGAffineTransformMakeRotation(angle));
+    return label;
 }
 @end
